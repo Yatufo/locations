f42b1d8c016eb8fa281da4e4311f713aeeda6292
Alex <project.demonio@gmail.com>
2017-08-01 10:29:16 -0400

    Updating the timestamp only the first time

--- js/mongo/updateDailyVisibility.js
+++ js/mongo/updateDailyVisibility.js
@@ -16,10 +16,13 @@ function updateDailyVisibility() {
   // only the estates with updated will be fully updated and visible.
   db.updates.find({ updated : true, timestamp : LAST_BATCH_START })
   .forEach(function(updatedEstate){
+    var timestamp = new Date(updatedEstate.timestamp);
     delete updatedEstate._id
+    delete updatedEstate.timestamp
+    delete updatedEstate.updated
+
     updatedEstate.calculated = { visible : true};
-    updatedEstate.timestamp = new Date(updatedEstate.timestamp);
-    db.estates.update({ id : updatedEstate.id}, {$set : updatedEstate}, {upsert : true});
+    db.estates.update({ id : updatedEstate.id}, {$set : updatedEstate, $setOnInsert : {timestamp : timestamp}}, {upsert : true});
   });
 
   //the rest will be just have the timestamp updated and also its visibility.
