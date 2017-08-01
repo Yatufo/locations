38265edb718d8c9bcf0b56895264ff3dec019947
Alex <project.demonio@gmail.com>
2017-08-01 11:09:00 -0400

    removing files after finishing

--- runner.sh
+++ runner.sh
@@ -15,3 +15,8 @@ echo 4. Exporting query to csv:
 export EXPORTS_QUERY='{ "calculated.distances": { "$exists": true }, "price": {"$lt": 700000}, "revenue": {"$gt": 0}, "units.residential": { "$gt": 2 }, "units.commercial": 0, "calculated.visible"  : true}'
 
 mongoexport --db realestate --collection estates --type=csv --query $EXPORTS_QUERY --fieldFile data/exportFields.txt --out data/exports/prospects.csv
+
+
+echo 5. Cleaning up
+rm data/updates.json
+rm data/grid.json
