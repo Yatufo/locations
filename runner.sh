f2b5234a6235ee2be5cecd11c04a18c24ef562f7
Alex <project.demonio@gmail.com>
2017-08-01 14:21:24 -0400

    Formatting runner.sh

--- runner.sh
+++ runner.sh
@@ -5,7 +5,8 @@ echo 1. Running the scraper:
 ./protractor-headless.sh protractor.conf.js
 
 echo 2. Importing the scraped data
-mongoimport -h localhost:27017 --db realestate --collection updates --mode upsert --upsertFields id,timestamp  --jsonArray data/updates.json
+mongoimport -h localhost:27017 --db realestate --collection updates \
+  --mode upsert --upsertFields id,timestamp  --jsonArray data/updates.json
 
 
 echo 3. Calculating important data in Mongo:
@@ -15,7 +16,8 @@ echo 4. Exporting query to csv:
 
 export EXPORTS_QUERY='{ "calculated.distances": { "$exists": true }, "price": {"$lt": 700000}, "revenue": {"$gt": 0}, "units.residential": { "$gt": 2 }, "units.commercial": 0, "calculated.visible"  : true}'
 
-mongoexport --db realestate --collection estates --type=csv --query $EXPORTS_QUERY --fieldFile data/exportFields.txt --out data/exports/prospects.csv
+mongoexport --db realestate --collection estates --type=csv \
+--query $EXPORTS_QUERY --fieldFile data/exportFields.txt --out data/exports/prospects.csv
 
 
 echo 5. Cleaning up
