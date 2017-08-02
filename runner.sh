754265042152d6fe8312aefb77a167535e1af6b3
Alex <project.demonio@gmail.com>
2017-08-02 07:18:04 -0400

    Fixing the export

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# without docker: protractor protractor.conf.js
+without docker: protractor protractor.conf.js
 echo 1. Running the scraper:
 ./protractor-headless.sh protractor.conf.js
 
@@ -14,10 +14,8 @@ mongo < ./js/mongo/calculate.js
 
 echo 4. Exporting query to csv:
 
-export EXPORTS_QUERY='{ "calculated.distances": { "$exists": true }, "price": {"$lt": 700000}, "revenue": {"$gt": 0}, "units.residential": { "$gt": 2 }, "units.commercial": 0, "calculated.visible"  : true}'
-
 mongoexport --db realestate --collection estates --type=csv \
---query $EXPORTS_QUERY --fieldFile data/exportFields.txt --out data/exports/prospects.csv
+--query '{ "calculated.distances": { $exists: true }, price: {$lt: 700000}, revenue: {$gt: 0}, "units.residential": { "$gt": 2 }, "units.commercial": 0, "calculated.visible"  : true}' --fieldFile data/exportFields.txt --out data/exports/prospects.csv
 
 
 echo 5. Cleaning up
