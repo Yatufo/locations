e5b591a9fe3d23549dd2a74cbbe1d746caef3a96
Alex <project.demonio@gmail.com>
2017-08-05 15:13:53 -0400

    Reinverting propertly the labels

--- runner.sh
+++ runner.sh
@@ -22,11 +22,11 @@ mongoexport --db realestate --collection estates --type=csv \
 --fieldFile data/exportFields.txt --out data/prospects.csv
 
 echo 5. Rename the csv file titles:
-export CURRENT_LABELS=location.coordinates.0,location.coordinates.1,id,price,revenue,calculated.ratio,url,residentialUnits,score,timestamp,calculated.recent
+export CURRENT_LABELS=location.coordinates.1,location.coordinates.0,id,price,revenue,calculated.ratio,url,residentialUnits,score,timestamp,calculated.recent
 
-export NEW_LABELS=lat,lon,id,price,revenue,ratio,url,residentialUnits,score,timestamp,recent
+export NEW_LABELS=lon,lat,id,price,revenue,ratio,url,residentialUnits,score,timestamp,recent
 
-sed -e "s/$CURRENT_LABELS/$NEW_LABELS/" data/prospects.csv > data/results/prospects-labels.csv
+sed -e "s/$CURRENT_LABELS/$NEW_LABELS/" data/prospects.csv > data/prospects-labels.csv
 
 
 echo 5. Cleaning up
