f1a55198df524f7e881f777bf5dc27aeeaf7284b
Alex <project.demonio@gmail.com>
2017-08-05 15:35:49 -0400

    Init script adding start mongo

--- init.sh
+++ init.sh
@@ -1,11 +1,10 @@
 #! /bin/sh
 
-
-echo 1. Importing interests:
-mongoimport -h localhost:27017 --db realestate --collection interests --mode upsert --upsertFields name --jsonArray data/interests.json
-
-echo 2. Start mongo
+echo 1. Start mongo
 docker run --name centrisdb  -p 27017:27017  -d mongo
+
+echo 2. Importing interests:
+mongoimport -h localhost:27017 --db realestate --collection interests --mode upsert --upsertFields name --jsonArray data/interests.json
 
 echo 3. Setup mongo schema
 mongo < ./js/mongo/init.js
