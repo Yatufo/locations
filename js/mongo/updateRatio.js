ce07d727b6b5cd9ded3e8db0a5ea9092c66d69a7
Alex <project.demonio@gmail.com>
2017-08-09 15:00:45 -0400

    Calculating the ratios using the tax information

--- js/mongo/updateRatio.js
+++ js/mongo/updateRatio.js
@@ -2,15 +2,20 @@
 function updateCalculatedRatio() {
   // Setting the calculated data
   db.estates.find({}).forEach(function(estate) {
-    var ratio = Math.round((estate.revenue / estate.price) * 100)
+    var valuation = estate.evaluationBuilding + estate.evaluationLot
+    var taxes = estate.taxMunicipal + estate.taxSchool;
+    var insurance = 0.006 * estate.price;
+
+
+    var cashflow = estate.revenue - taxes - insurance;
+    var yyield = Math.round((cashflow * 100)/ estate.price) || -1;
+    var taxRate = (valuation && taxes) ? taxes / valuation : -1;
+    var capRate = Math.round((estate.revenue/estate.price) * 100)
 
     db.estates.update({_id: estate._id}, {
-      $set: { 'calculated.ratio': ratio }
+      $set: { "calculated.capRate": capRate,  "calculated.yield": yyield, "calculated.taxRate" : taxRate}
     });
   });
 }
-
-
-
 
 db.system.js.save({_id: 'updateCalculatedRatio', value: updateCalculatedRatio});
