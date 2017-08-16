7eeb865009f86e3c2414ba5b42cc75eae5e2a2f3
Alex <project.demonio@gmail.com>
2017-08-16 09:54:49 -0400

    Adding the sinceInDays calculated property

--- js/mongo/updateRatio.js
+++ js/mongo/updateRatio.js
@@ -5,15 +5,19 @@ function updateCalculatedRatio() {
     var valuation = estate.evaluationBuilding + estate.evaluationLot
     var taxes = estate.taxMunicipal + estate.taxSchool;
     var insurance = 0.006 * estate.price;
+    var mortgage = 0.046 * estate.price;
 
 
-    var cashflow = estate.revenue - taxes - insurance;
+    var cashflow = estate.revenue - taxes - insurance - mortgage;
     var yyield = Math.round((cashflow * 100)/ estate.price) || -1;
     var taxRate = (valuation && taxes) ? taxes / valuation : -1;
     var capRate = Math.round((estate.revenue/estate.price) * 100)
+    var sinceInDays = Math.round((new Date().getTime() - estate.timestamp) / (24 * 60 * 60 * 1000));
 
     db.estates.update({_id: estate._id}, {
-      $set: { "calculated.capRate": capRate,  "calculated.yield": yyield, "calculated.taxRate" : taxRate}
+      $set: { "calculated.capRate": capRate,  "calculated.yield": yyield, "calculated.taxRate" : taxRate,
+       "calculated.sinceInDays": sinceInDays
+    }
     });
   });
 }
